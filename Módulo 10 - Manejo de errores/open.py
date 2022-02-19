b66e3476ca9dead64c8de07f7d307d4888f5d132
Ruben Garcia <99065184+GITRubenGarcia@users.noreply.github.com>
2022-02-18 23:07:12 -0700

    Primer commit - Curso de introducción Python
    Introduje las katas del curso de introducción de Python

---
+++ Módulo 10 - Manejo de errores/open.py
@@ -0,0 +1,8 @@
+def main():
+    open("/path/to/mars.jpg")
+
+if __name__ == '__main__':
+    main()
+
+#Intenta crear un archivo de Python y asígnale el nombre *open.py*, con el contenido siguiente:
+
